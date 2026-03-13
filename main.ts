e9d19eb9c6a0795fbd15f457f248fce6a639f9a4
1120129-art <1120129@kids.jgjhs.tyc.edu.tw>
2026-03-13 13:28:27 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,53 @@
+let _ = 0
+input.onGesture(Gesture.Shake, function () {
+    _ = randint(1, 6)
+    if (_ == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (_ == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . . . . .
+            . . # . .
+            . . . . .
+            `)
+    } else if (_ == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (_ == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (_ == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (_ == 6) {
+        basic.showLeds(`
+            . . . . .
+            # . # . #
+            . . . . .
+            # . # . #
+            . . . . .
+            `)
+    }
+})
